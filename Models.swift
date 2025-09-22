b30fff30c83014d9b3d971509e15033434b4e6f2
Jeffrey Berdeal <jpberdeal@comcast.net>
2025-09-21 20:13:56 -0400

    Add files via upload

---
+++ Models.swift
@@ -0,0 +1,41 @@
+//
+//  Models.swift
+//  Codepath-HW2
+//
+//  Created by Jeffrey Berdeal on 9/16/25.
+//
+
+import Foundation
+import ParseSwift
+
+struct AppUser: ParseUser {
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+    
+    var username: String?
+    var password: String?
+    var email: String?
+    var emailVerified: Bool?
+    var authData: [String : [String : String]?]?
+    
+    init() {}
+}
+
+struct Post: ParseObject {
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+
+    var author: AppUser?
+    var caption: String?
+    var image: ParseFile?
+    
+    var location: ParseGeoPoint?
+    
+    init() {}
+}
